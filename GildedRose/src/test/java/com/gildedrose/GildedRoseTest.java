93f386895df509bbc8c189a2df4354975d3f8d63
Leo Fontaine <leo.fontaine4@orange.fr>
2026-03-11 14:35:47 +0100

    test coverage quasi 100%, uniquement pas le toString de test

--- GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,117 @@
 package com.gildedrose;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void testStandardItemUpdates() {
+        // Test dégradation normale
+        Item[] items = new Item[] { new Item("Normal Item", 10, 20) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals(9, items[0].sellIn);
+        assertEquals(19, items[0].quality);
     }
 
+    @Test
+    void testStandardItemDegradesDoubleAfterSellIn() {
+        // Test dégradation double après péremption
+        Item[] items = new Item[] { new Item("Normal Item", 0, 20) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(18, items[0].quality);
+    }
+
+    @Test
+    void testQualityNeverNegative() {
+        // La qualité ne descend jamais sous 0
+        Item[] items = new Item[] { new Item("Normal Item", 0, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, items[0].quality);
+    }
+
+    @Test
+    void testAgedBrieIncreasesInQuality() {
+        // Le Aged Brie augmente en qualité
+        Item[] items = new Item[] { new Item("Aged Brie", 10, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(11, items[0].quality);
+    }
+
+    @Test
+    void testAgedBrieQualityMax50() {
+        // La qualité max est 50
+        Item[] items = new Item[] { new Item("Aged Brie", 10, 50) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(50, items[0].quality);
+    }
+
+    @Test
+    void testAgedBrieIncreasesDoubleAfterSellIn() {
+        // Le Aged Brie augmente de 2 après la date de péremption
+        Item[] items = new Item[] { new Item("Aged Brie", 0, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(12, items[0].quality);
+    }
+
+    @Test
+    void testSulfurasNeverChanges() {
+        // Sulfuras ne change jamais
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 10, 80) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(10, items[0].sellIn);
+        assertEquals(80, items[0].quality);
+    }
+
+    @Test
+    void testBackstagePassesNormal() {
+        // Plus de 10 jours : +1 qualité
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(21, items[0].quality);
+    }
+
+    @Test
+    void testBackstagePassesMediumUrgency() {
+        // 10 jours ou moins : +2 qualité
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(22, items[0].quality);
+    }
+
+    @Test
+    void testBackstagePassesHighUrgency() {
+        // 5 jours ou moins : +3 qualité
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(23, items[0].quality);
+    }
+
+    @Test
+    void testBackstagePassesExpired() {
+        // Après le concert : qualité tombe à 0
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, items[0].quality);
+    }
+
+    @Test
+    void testBackstagePassesQualityMax50() {
+        // Test de la limite à 50 même avec les bonus
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(50, items[0].quality);
+    }
 }
